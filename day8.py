6471a85b8e6de86a1b1a6cd953533c125b06da1c
Amila Ferron <aferron@pdx.edu>
2020-12-19 21:52:16 -0800

    Day 8 solution

--- day8.py
+++ day8.py
@@ -12,92 +12,105 @@ class instruct:
     def display(self):
         print(self.function, " ", self.value, " V: ", self.visited)
 
-def readIn():
-    count = 0
-    readLines = []
-    instructions = []
 
-    with open('da8.txt', 'r') as f:
-        Lines = f.readlines()
-        for line in Lines:
-            readline = line.replace('\\', '').strip()
-            ins = readline.split(' ')
-            instruction = instruct(ins[0], int(ins[1]))
-            instructions.append(instruction)
-            count += 1
-    return instructions
 
-def part1(instructions):
-    accumulator = 0
-    index = 0
-    ins = instructions[index]
+class solution:
+    def __init__(self, part1path=[]):
+        self.part1path = part1path
 
-    while ins.visited is False:
-        ins.visited = True
-        if ins.function == 'acc':
-            accumulator += ins.value
-            index += 1
-        elif ins.function == 'jmp':
-            index += ins.value
-        elif ins.function == 'nop':
-            index += 1
-        else:
-            print('error')
-            break
+    def readIn(self):
+        count = 0
+        readLines = []
+        instructions = []
+
+        with open('da8.txt', 'r') as f:
+            Lines = f.readlines()
+            for line in Lines:
+                readline = line.replace('\\', '').strip()
+                ins = readline.split(' ')
+                instruction = instruct(ins[0], int(ins[1]))
+                instructions.append(instruction)
+                count += 1
+        return instructions
+
+    def part1(self, instructions):
+        accumulator = 0
+        index = 0
         ins = instructions[index]
 
-    return accumulator
-
-def part2(instructions):
-    accumulator = 0
-    index = 0
-    ins = instructions[index]
-    length = len(instructions)
-    print("length:",length)
-    c = 0
-    changed = False
-
-    for i in range(0,length):
         while ins.visited is False:
             ins.visited = True
+            self.part1path.append(ins)
             if ins.function == 'acc':
                 accumulator += ins.value
                 index += 1
             elif ins.function == 'jmp':
-                if c == i and changed is False:
-                    ins.function = 'nop'
-                    changed = True
-                    ins.visited = False
-                    print("changed at c =", c)
-                else:
-                    index += ins.value
-                    c += 1
+                index += ins.value
             elif ins.function == 'nop':
-                if c == i and changed is False:
-                    ins.function = 'jmp'
-                    ins.visited = False
-                    changed = True
-                    print("changed at c =", c)
-                else:
-                    index += 1
-                    c += 1
+                index += 1
+            else:
+                print('error')
+                break
+            ins = instructions[index]
+
+        return accumulator
+
+    def reachEnd(self, instructions, ind, acc):
+        accumulator = acc
+        index = ind
+        ins = instructions[index]
+        length = len(instructions)
+        visits = [False] * length
+
+        while visits[index] is False:
+            visits[index] = True
+            if ins.function == 'acc':
+                accumulator += ins.value
+                index += 1
+            elif ins.function == 'jmp':
+                index += ins.value
+            elif ins.function == 'nop':
+                index += 1
             else:
                 print('error')
                 break
             if index == length:
-                return accumulator
+                print("final value when end reached is: ", accumulator)
+                return True
             ins = instructions[index]
+
+        return False
+
+
+    def startAndSwitch(self, instructions):
         accumulator = 0
         index = 0
-        ins = instructions[0]
-        c = 0
-        changed = False
-        for x in instructions:
-            x.visited = False
+        ins = instructions[index]
+        length = len(instructions)
+        visits = [False] * length
+
+        while visits[index] is False:
+            visits[index] = True
+            if ins.function == 'acc':
+                accumulator += ins.value
+                index += 1
+            elif ins.function == 'jmp':
+                if self.reachEnd(instructions, index + 1, accumulator) is True:
+                    return True
+                index += ins.value
+            elif ins.function == 'nop':
+                if self.reachEnd(instructions, index + ins.value, accumulator) is True:
+                    return True
+                index += 1
+            else:
+                print('error')
+                break
+            ins = instructions[index]
+
+        return False 
 
 
-    return -1
 
-read = readIn()
-print(part2(read))
-        
+solved = solution()
+read = solved.readIn()
+print(solved.startAndSwitch(read))
